c42e959a6aa4796eed4e9b27216e108a2d1b2d5a
kornev.a <kornev.a@qsoft.ru>
2021-08-12 19:01:06 +0300

    newReposytory

---
+++ src/2_Task/state.js
@@ -0,0 +1,2 @@
+export const oneMessage = 'oneMessage';
+export const twoMessage = 'twoMessage';
---
+++ src/state/state.js
@@ -0,0 +1,69 @@
+export default {
+  products: [
+    {
+      title: 'Радионяня Motorola MBP16',
+      price: 3_690,
+      img: './img/radio.jpg',
+    },
+    {
+      title: 'img/toothbrush.jpg',
+      price: 5_660,
+      img: './img/toothbrush.jpg',
+    },
+    {
+      title: 'Мобильный телефон Xiaomi Poco X3 NFC 6/128GB',
+      price: 18_990,
+      img: './img/x248_trim.png',
+    },
+    {
+      title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+      price: 21_790,
+      img: './img/phone.jpg',
+    },
+    {
+      title: 'Электроскейт Razor Cruiser',
+      price: 24_690,
+      img: './img/board.jpg',
+    },
+    {
+      title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+      price: 14_960,
+      img: './img/phone-2.jpg',
+    },
+    {
+      title: 'Смартфон Xiaomi Redmi 6/128GB',
+      price: 8_960,
+      img: './img/phone-3.jpg',
+    },
+    {
+      title: './img/bicycle.jpg',
+      price: 39_900,
+      img: './img/bicycle.jpg',
+    },
+    {
+      title: 'Гироскутер Razor Hovertrax 2.0',
+      price: 34_900,
+      img: './img/wheels.jpg',
+    },
+    {
+      title: 'Детский трюковой самокат Razor Grom',
+      price: 4_990,
+      img: './img/scooter.jpg',
+    },
+    {
+      title: 'Роллерсёрф Razor RipStik Air Pro',
+      price: 6_690,
+      img: './img/ripstik.jpg',
+    },
+    {
+      title: 'Наушники AirPods с беспроводным зарядным футляром',
+      price: 16_560,
+      img: 'img/airpods.jpg',
+    },
+    {
+      title: 'Наушники Sony',
+      price: 30_690,
+      img: 'img/headphones.jpg',
+    },
+  ],
+};
